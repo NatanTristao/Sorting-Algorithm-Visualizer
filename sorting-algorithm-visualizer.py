a7e3b3793d6d52092aba720c0699906e311998b1
Natan Tristao <natanftristao@gmail.com>
2026-02-04 09:27:49 -0300

    Implementing insertion sort

--- sorting-algorithm-visualizer.py
+++ sorting-algorithm-visualizer.py
@@ -1,3 +1,4 @@
+#works only for in-place sorting algorithms
 import pygame
 import random
 import math
@@ -103,6 +104,24 @@ def bubble_sort(draw_info, ascending=True): #our bubble sort is a generator
 def insertion_sort(draw_info, ascending=True):
 	lst = draw_info.lst
 
+	for i in range(1, len(lst)):
+		current = lst[i]
+
+		while True:
+			ascending_sort = i > 0 and lst[i - 1] > current and ascending
+			descending_sort = i > 0 and lst[i - 1] < current and not ascending
+
+			if not ascending_sort and not descending_sort:
+				break
+
+			lst[i] = lst[i - 1]
+			i = i - 1
+			lst[i] = current
+			draw_list(draw_info, {i - 1:draw_info.GREEN, i: draw_info.RED}, True)
+			yield True
+
+	return lst
+
 
 def main():
 	run = True
@@ -152,6 +171,12 @@ def main():
 				ascending = True
 			elif event.key == pygame.K_d and not sorting:
 				ascending = False
+			elif event.key == pygame.K_i and not sorting:
+				sorting_algo_name = "Insertion Sort"
+				sorting_algorithm = insertion_sort
+			elif event.key == pygame.K_b and not sorting:
+				sorting_algo_name = "Bubble Sort"
+				sorting_algorithm = bubble_sort
 
 	pygame.quit()
 
